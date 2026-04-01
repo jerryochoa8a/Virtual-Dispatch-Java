5dd1db50a6f8067c9aebad7c92bc9cf9b30c78e3
Jerry <jerryochoa8a@aol.com>
2026-03-31 23:13:15 -0700

    Changed Nil and Cons

--- Cons.java
+++ Cons.java
@@ -11,7 +11,7 @@ public class Cons implements ImmutableList {
 
     public boolean equals(final Object other) {
         if (other instanceof Cons) {
-            final Cons otherCons = (Cons) other;
+            final Cons otherCons = (Cons)other;
             return head == otherCons.head && tail.equals(otherCons.tail);
         } else {
             return false;
@@ -25,20 +25,40 @@ public class Cons implements ImmutableList {
     public int hashCode() {
         return sum();
     } // hashCode
+
+    // [4, 2, 7].length() ==> 3
+    // head: 4
+    // tail: [2, 7]
+    // this: [4, 2, 7]
+    // expected result: 3
+    public int length() {
+        // must be recursive - Cons is recursive at the data level
+        // [2, 7].length() ==> 2
+        // rest: 2
+        int rest = tail.length();
+//        System.out.println("REST: " + rest + " :REST DONE");
+        return rest + 1;
+        // NEXT TIME: isEmpty, addAmount
+    }
+
+    public int sum() {
+        return head+tail.sum();
+    }
+    // [].sum()
+    // head plus the rest of the body gives you the sum
+
+    public boolean contains(final int value) {
+        return head==value || tail.contains(value);
+    }
+    // returns the value if it equals the head OR if its a value in the rest of the list. - Jerry
+
+    public ImmutableList append(final ImmutableList other) {
+        return new Cons(head, tail.append(other));
+    }
+    // changed return null to "return new Cons(head, tail.append(other))".
+    // keeping the current head and making "other" to the tail and then making a new Cons
+    //if tail == cons --> keep looping
+    //if tail == nil --> return other
+
+
 } // Cons
-
-
-// ----------- new code ------------
-// @Override
-// public int length() { return 1 + tail.length(); }
-
-// @Override
-// public int sum() { return head + tail.sum(); }
-
-// @Override
-// public boolean contains(int value) { return head == value ||
-// tail.contains(value); }
-
-// @Override
-// public ImmutableList append(ImmutableList other) { return new Cons(head,
-// tail.append(other)); }
--- Nil.java
+++ Nil.java
@@ -1,6 +1,5 @@
 public class Nil implements ImmutableList {
-    public Nil() {
-    }
+    public Nil() {}
 
     public boolean equals(final Object other) {
         return other instanceof Nil;
@@ -13,18 +12,22 @@ public class Nil implements ImmutableList {
     public int hashCode() {
         return 0;
     } // hashCode
+
+    public int length() {
+        return 0;
+    }
+
+    public int sum() {
+        return 0;
+    }//changed from -1 to 0
+
+    public boolean contains(final int value) {
+        return false;
+    }
+    // return false insted of true becouse it would return false after reaching
+    // the end of the list meaning the value was not found. -jerry
+
+    public ImmutableList append(final ImmutableList other) {return other;}
+    //Changed return null to return other. appending to an empty list should just return the other list - Jerry
+
 } // Nil
-
-
-// ----------- new code ------------
-// @Override
-// public int length() { return 0; }
-
-// @Override
-// public int sum() { return 0; }
-
-// @Override
-// public boolean contains(int value) { return false; }
-
-// @Override
-// public ImmutableList append(ImmutableList other) { return other; }
